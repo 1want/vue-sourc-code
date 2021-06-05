2c0c23a0b6a6e09a11b75a817a6e26e2054dd8dc
1want <56565484@qq.com>
2021-06-05 11:19:29 +0800

    添加h,vnode

--- diff/src/index.js
+++ diff/src/index.js
@@ -1,25 +1,9 @@
-import {
-  init,
-  classModule,
-  propsModule,
-  styleModule,
-  eventListenersModule,
-  h
-} from 'snabbdom'
+import h from './snabbdom/h'
 
-const patch = init([
-  // Init patch function with chosen modules
-  classModule, // makes it easy to toggle classes
-  propsModule, // for setting properties on DOM elements
-  styleModule, // handles styling on elements with support for animations
-  eventListenersModule // attaches event listeners
+const p1 = h('ul', {}, [
+  h('li', {}, '苹果'),
+  h('li', {}, '香蕉'),
+  h('li', {}, [h('span', {}, 'A'), h('span', {}, 'B')])
 ])
 
-const container = document.getElementById('container')
-
-const p1 = h('ul', { class: { box: true } }, [
-  h('li', '苹果'),
-  h('li', '香蕉'),
-  h('a', { props: { href: 'http://www.baidu.com' } }, '百度')
-])
-patch(container, p1)
+// console.log(p1)
---
+++ diff/src/snabbdom/h.js
@@ -0,0 +1,22 @@
+import vnode from './vnode'
+
+export default function (sel, data, c) {
+  if (arguments.length !== 3) {
+    throw new Error('h函数必须传三个参数')
+  }
+  if (typeof c === 'string' || typeof c === 'number') {
+    return vnode(sel, data, undefined, c, undefined)
+  } else if (Array.isArray(c)) {
+    let children = []
+    c.forEach(item => {
+      if (!(typeof item === 'object' && item.hasOwnProperty('sel')))
+        throw new Error('传入的数组参数中有项不是h函数')
+      children.push(item)
+    })
+    return vnode(sel, data, children, undefined, undefined)
+  } else if (typeof c === 'object' && c.hasOwnProperty('sel')) {
+    console.log(c)
+    let children = [c]
+    return vnode(sel, data, children, undefined, undefined)
+  }
+}
